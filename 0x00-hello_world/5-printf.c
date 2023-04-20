428d2a3e4528ec5b456d6375269b4089fad5b136
fikir <fikirfikre15@gmail.com>
2023-04-20 09:10:25 -0700

    fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,12 @@
 
 /**
  * main - it will print "with proper grammer, but the outcome is a 
- * piece of art, followed by a news line
+ *                         piece of art, followed by a news line.
  *
- * Return: 0
+ * Return: 0.
  */
-
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
