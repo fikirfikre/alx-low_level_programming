85139cf575e9f3e6b0f86158d616ba03a46b326d
fikir <fikirfikre15@gmail.com>
2023-04-20 07:50:58 -0700

    Fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+/*
+ *main - Entry point
+ *
+ *Return : Always 0
+ */
+
 
 int main(void)
 {
-  puts("\"Programming is like building a multilngual puzzle");
-
+  /*it will print the value*/
+  
+  puts("\"Programming is like building a multilngual puzzle\n");
     return 0;
 }
