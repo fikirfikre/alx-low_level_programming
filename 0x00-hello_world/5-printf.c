6966b3eba1f95ebdbf6eba195255309e77fa53c2
fikir <fikirfikre15@gmail.com>
2023-04-20 09:02:19 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - it will print "with proper grammer, but the outcome is a 
+ * piece of art, followed by a news line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+   printf("with proper grammer, but the outcome is a piece of art, \n");
+   return (0);
+}
