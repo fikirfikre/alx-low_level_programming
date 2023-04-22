9f095ae962f17ca6e462468b55a160c7f971c98f
fikir <fikirfikre15@gmail.com>
2023-04-22 09:17:30 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+/**
+ * main - it is a program that will display the alphabet in lowercase, followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	c = 'a';
+        for (; c <= 'z'; c++)
+		putchar(c);
+	        
+	putchar('\n');
+	return (0);
+}
+ 
