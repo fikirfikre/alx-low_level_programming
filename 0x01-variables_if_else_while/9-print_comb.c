4fd8658ea8517352da71dc1fed27675101d59a83
fikir <fikirfikre15@gmail.com>
2023-04-22 11:36:47 -0700

    fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,16 @@ int main(void)
 {
   int c;
 
-  for (c=0; c < 9; c++)
+  for (c = 0; c <= 9; c++)
     {
       putchar(c + '0');
-      putchar(',');
-      putchar(' ');
+      if (c != 9)
+	{
+	  putchar(',');
+	  putchar(' ');
+	}
+     
     }
-  putchar(c + '0');
+  putchar('\n');
   return (0);
 }
