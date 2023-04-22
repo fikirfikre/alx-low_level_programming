2388a2e6de27911936faf83d78d256bc72c00f42
fikir <fikirfikre15@gmail.com>
2023-04-22 09:44:13 -0700

    style fixed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
-#include  <stdio.h>
+#include <stdio.h>
 /**
- * main - it is a program that will display the alphabet in lowercase,
- * followed by a new line
- *
- * Return: 0
- */
+* main - it is a program that will display the alphabet in lowercase,
+* followed by a new line
+*
+* Return : 0
+*/
 int main(void)
 {
-	char c;
-	c = 'a';
-	
-        for (; c <= 'z'; c++)
-	  putchar(c);
-	        
-	putchar('\n');
-	return (0);
+char c;
+
+c = 'a';
+for(; c <= 'z'; c++)
+    putchar(c);
+putchar('\n');
+return (0);
 }
+
