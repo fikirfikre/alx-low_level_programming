e71a6ee899a75eae2f51457d3007e5c176e798e9
Fikir Fikre <fikirfikre15@gmail.com>
2023-04-28 11:05:55 +0300

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - print 10 times the numbers from 0 ot 14,
+ * followed by a new line
+ */
+void more_numbers(void)
+{
+	 int i;
+	 int j;
+
+	 
+	 i = 0;
+	 
+	 while(i < 15)
+	 {
+	 	for(j = 0; j < 15; j++)
+	 	{
+	 		if (j > 9)
+	 			_putchar(j/10 + '0');
+	 		_putchar(j%10 + '0');	
+	 
+	 	}	 
+		_putchar('\n');
+	 	i++;
+	 }
+}
