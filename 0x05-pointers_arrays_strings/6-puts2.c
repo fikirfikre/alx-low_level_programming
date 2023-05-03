35a282df89b99f0556bf794499c0823d18d93ae8
Fikir Fikre <fikirfikre15@gmail.com>
2023-05-03 10:41:53 +0300

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every even character of a string,starting with the first character,
+ * followed by a new line.
+ * @str: a string
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i;
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
