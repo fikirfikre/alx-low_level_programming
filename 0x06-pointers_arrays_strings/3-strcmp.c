e68ab890a094f82185da3530a05e5ce5aa6ce750
Fikir Fikre <fikirfikre15@gmail.com>
2023-05-04 17:42:00 +0300

    updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,33 +8,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len1, len2, result;
-	char *p1;
-	char *p2;
-
-	len1 = 0;
-	len2 = 0;
-	p1 = s1;
-	p2 = s2;
-	while (s1[len1] != '\0')
-		len1++;
-	while (s2[len2] != '\0')
-		len2++;
-	if (len1 > len2)
-		result = 15;
-	else if (len2 > len1)
-		result = -15;
-	else
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		while (*p1 != '\0')
-		{
-			if (*p1++ > *p2++)
-				result = 15;
-			else if (*p1++ < *p2++)
-				result = 15;
-		}
-		if ((*p1 == '\0') && (*p1 == *p2))
-			result = 0;
+		s1++;
+		s2++;
 	}
-	return (result);
+	if (*s1 < *s2)
+		return (*s1 - *s2);
+	if (*s1 > *s2)
+		return (*s1 - *s2);
+	else
+		return (0);
 }
