948e104b6d60d2000d9c24e00c706a0b7d3c2af0
Fikir Fikre <fikirfikre15@gmail.com>
2023-05-09 15:49:18 +0300

    updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,11 +9,20 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int len;
+	int i, j;
 
 	len = 0;
+	i = 0;
 	if ( *s == '\0' || *accept == '\0')
 		return (len);
-	while(*s && _strchr(accept,*s++))
-		len++;
+	while (s[i] != ' ' && s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				len++;
+		}
+		i++;
+	}
 	return (len);
 }
