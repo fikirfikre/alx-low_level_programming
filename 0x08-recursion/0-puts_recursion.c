fe68e34edcde376e1a292025e38195ffd84da6d2
Fikir Fikre <fikirfikre15@gmail.com>
2023-05-11 22:42:40 +0300

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,15 @@
  */
 void _puts_recursion(char *s)
 {
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
 	if (*s == '\0')
+	{
+		_putchar(' ');
 		_putchar('\n');
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+	}
 }
