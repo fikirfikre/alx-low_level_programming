aa9ad65625da701857e0f62e8e00d23ad403b190
Fikir Fikre <fikirfikre15@gmail.com>
2023-05-12 14:58:21 +0300

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - it print its file name, followed by a line
+ * @argc: length of argv array
+ * @argv: and arry of argument
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s",argv[argc * 0]);
+	printf("\n");
+	return (0);
+}
