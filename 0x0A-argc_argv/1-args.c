b5bd2fca2b061794f1e3998e0767e89514f584f1
Fikir Fikre <fikirfikre15@gmail.com>
2023-05-12 15:11:27 +0300

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print the number of argument passed into it
+ * @argc: number of argument in argv
+ * @argv: an array of argument
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		while(*argv++)
+			i++;
+		printf("%d\n", i - 1);	
+	}
+	return (0);
+}
