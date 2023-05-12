3f8bde0c5abc073207a3120d76bd5763d0f1d96a
Fikir Fikre <fikirfikre15@gmail.com>
2023-05-12 16:07:02 +0300

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiples two numbers
+ * @argc: length of the argument
+ * @argv: array of the argument
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int mul, i;
+
+	mul = 1;
+	i = 1;
+	if (argc <= 2 || argc >= 4)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{ 
+		while (i < 3)
+		{
+			mul = mul * atoi(argv[i]);
+			i++;
+		}
+		printf("%d",mul);	
+	}
+	return (0);
+}		
