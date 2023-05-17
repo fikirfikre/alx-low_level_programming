72c604d0170d059f25a469843f5a4ac313d0115c
Fikir Fikre <fikirfikre15@gmail.com>
2023-05-17 19:17:20 +0300

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,16 +14,16 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	p = malloc(((len1 + len2) + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
-	_strncat(p,s1,len1);
-	_strncat(p,s2,len2);
+	_strncat(p, s1, len1);
+	_strncat(p, s2, len2);
 	p += '\0';
 	return (p);
 }
